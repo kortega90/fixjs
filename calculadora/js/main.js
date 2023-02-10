3f2236706b14626637e11650c355ad50de4aa978
Kengi Ortega <kortega90@hotmail.com>
2023-02-09 21:32:18 -0300

    final

--- calculadora/js/main.js
+++ calculadora/js/main.js
@@ -2,11 +2,11 @@ const input1 = document.querySelector('[name="number1"]');
 input1.addEventListener("change", handleInput1);
 
 
-async function handleInput1(event) {
+function handleInput1(event) {
  let  n1 = parseFloat(event.target.value);
-  console.log(n1);
   if (isNumber(n1)) {
     event.target.classList.remove("input-error");
+    return n1;
   } else {
     event.target.classList.add("input-error");
   }
@@ -15,12 +15,12 @@ async function handleInput1(event) {
 const input2 = document.querySelector('[name="number2"]');
 input2.addEventListener("change", handleInput2);
 
-let n2 = 0;
-async function handleInput2(event) {
+
+function handleInput2(event) {
   let n2 = parseFloat(event.target.value);
-  console.log(n2);
   if (isNumber(n2)) {
     event.target.classList.remove("input-error");
+    return n2;
   } else {
     event.target.classList.add("input-error");
   }
@@ -30,27 +30,46 @@ async function handleInput2(event) {
 const btnSum = document.querySelector('[id="btn-plus"]');
 btnSum.addEventListener("click", handlebtnSum);
 
-
-async function handlebtnSum(event) {
+function handlebtnSum(event) {
   let char = '+';
-  console.log(char);
+
+if (char == "+"){
+    total =  parseFloat(input1.value) + parseFloat(input2.value);
+}
+const resultBox = document.querySelector('[id="result-box"]');
+resultBox.innerHTML = total;
+
 }
 
 //-----------------------------------------------------------------
-async function valorTotal(){
-    const valtotal = await calculador(n1,n2);
-    console.log(valtotal);
+const btnMult = document.querySelector('[id="btn-times"]');
+btnMult.addEventListener("click", handleBtnMult);
+
+function handleBtnMult(event) {
+  let char = '*';
+
+if (char == "*"){
+    total =  parseFloat(input1.value) * parseFloat(input2.value);
+}
+const resultBox = document.querySelector('[id="result-box"]');
+resultBox.innerHTML = parseFloat(total);
+
+}
+//-----------------------------------------------------------------
+const btnClear = document.querySelector('[id="btn-clear"]');
+btnClear.addEventListener("click", handleBtnClear);
+
+function handleBtnClear(event) {
+  let char = 'C';
+
+if (char == "C"){
+  input1.value = null;
+  input2.value = null;
+  const resultBox = document.querySelector('[id="result-box"]');
+  resultBox.innerHTML = parseFloat(0);
+}
 }
 
-
-async function calculador(n1,n2) {
-   if (char == "+"){
-    total =  n1 + n2;
-}
- else{
-     console.log("error")
-}
-}
 
 //----------------------------------------------------------------//
 function isNumber(n) {
